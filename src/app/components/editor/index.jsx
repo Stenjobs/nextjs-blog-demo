e7db7f7a9eb6433043d5020f246d5464aebea9aa
lixuejian <1305337942@qq.com>
2025-03-05 17:23:08 +0800

    feat:添加表情

--- src/app/components/editor/index.jsx
+++ src/app/components/editor/index.jsx
@@ -16,12 +16,13 @@ export default function TextEditor({ value, onChange }) {
         plugins: [
           'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
           'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-          'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+          'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount',
+          'emoticons'  // 添加表情插件
         ],
         toolbar: 'undo redo | blocks | ' +
           'bold italic forecolor | alignleft aligncenter ' +
           'alignright alignjustify | bullist numlist outdent indent | ' +
-          'removeformat | help',
+          'removeformat | emoticons | help',  // 在工具栏添加表情按钮
         content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
       }}
       value={value}
