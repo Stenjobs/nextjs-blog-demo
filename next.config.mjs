c64b5f0c0fc77949e37b60c9f3bd69e3686f5f65
lixuejian <1305337942@qq.com>
2025-03-07 14:38:43 +0800

    deploy添加

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,8 @@ const nextConfig = {
       {
         source: '/api/:path*',
         // destination: 'http://8.134.205.132:6677/api/:path*' // 替换为你的后端服务器地址
-        destination: 'http://localhost:3000/api/:path*' // 替换为你的后端服务器地址
+        // destination: 'http://localhost:3000/api/:path*' // 替换为你的后端服务器地址
+        destination: process.env.NEXT_PUBLIC_API_URL + '/api/:path*'
       }
     ]
   },
@@ -18,8 +19,18 @@ const nextConfig = {
   // 如果需要配置其他选项，例如：
   images: {
     // domains: ['8.134.205.132'],
-    domains: ['localhost'],
+    domains: [process.env.NEXT_PUBLIC_BASE_URL],
   }
+  ,
+  // // 配置静态文件导出
+  // output: 'export',
+  // // 静态导出时禁用图片优化
+  // images: {
+  //   ...nextConfig.images,
+  //   unoptimized: true
+  // },
+  // // 禁用 i18n
+  // i18n: null
 };
 
 export default nextConfig;
